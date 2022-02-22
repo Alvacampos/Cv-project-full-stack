874c61eddb9ebc927f7dad3251e68a73a91296cd
Gonzalo Alvarez Campos <Gonzalo.AlvarezCampos@endava.com>
2022-02-22 09:20:07 -0300

    more example code for devs

--- Front-End/README.md
+++ Front-End/README.md
@@ -5,3 +5,7 @@ Welcome to the vanilla stage of this project, here we will only use HTML, CSS an
 ## Installation Guide
 
 First do an npm install, yeah I know we talked that this was vanilla but we will use live-server to make the development a lot more friendly.
+
+## Live-server
+
+- npm run dev -> to run live-server and allow your vanilla app to have hot reload.
--- Front-End/index.js
+++ Front-End/index.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+// Esto es lo mas basico de JS, en la linea 2 se JS selecciona un elemento de HTML usando su ID muy parecido a como lo hace css, cuando JS logra "localizar" el elemento lo guarda en
+// la variable buttonTest y nos permite acceder a las funcionalidades que tiene.
+let buttonTest = document.getElementById('test-button');
+
+// Esto es un eventListener nos permite agregar una funcion que "escucha" a ese botton, y reacciona cuando "escucha" lo que le pedimos que escuche, en este caso es un click.
+// 'click' especifica que tiene que reaccionar a un click y despues sigue la funcion() y hacer un console.log
+// esto se va a disparar cada vez que se haga click en el boton
+buttonTest.addEventListener('click', function () {
+  console.log('asd');
+});
